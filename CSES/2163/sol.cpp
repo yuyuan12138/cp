619d0f0f84817c1793fa0a9fe7756998817acc2e
Ruojun Wang <yuyuan12138x@gmail.com>
2025-12-13 18:22:48 +0800

    update CSES

--- CSES/2163/sol.cpp
+++ CSES/2163/sol.cpp
@@ -3,10 +3,14 @@
  *    created: 2025-12-12 21:56:13
  **/
 #include <bits/stdc++.h>
-#include <ext/rope>
-using namespace __gnu_cxx;
 
 using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
 
 #ifdef LOCAL
 #include "algo/debug.h"
@@ -24,19 +28,21 @@ int main() {
     cout << 1;
     return 0;
   }
-  rope<int> a;
+  ordered_set st;
   for (int i = 1; i <= n; i++) {
-    a.push_back(i);
+    st.insert(i);
   }
-
-  int cur = k % int(a.size());
-  while (a.size() > 0) {
-    cout << a[cur] << ' ';
-    a.erase(cur, 1);
-    if (a.size() > 0) {
-      cur = (cur + k) % int(a.size());
+  
+  int cur = k % int(st.size());
+  while (st.size() > 0) {
+    auto it = st.find_by_order(cur);
+    cout << *it << ' ';
+    st.erase(it);
+    if (st.size() > 0) {
+      cur = (cur + k) % int(st.size());
     }
   }
+  cout << '\n';
   
   return 0;
 }
---
+++ CSES/2168/sol.cpp
@@ -0,0 +1,22 @@
+/**
+ *    author:  yuyuan
+ *    created: 2025-12-13 18:11:35
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
+
+
