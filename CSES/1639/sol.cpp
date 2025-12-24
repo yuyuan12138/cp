e252c0d0a0d1a87ec3e71d202e3d17733157f44b
yuyuan12138 <2797274057@qq.com>
2025-12-24 23:17:16 +0800

    update

---
+++ CSES/1639/sol.cpp
@@ -0,0 +1,30 @@
+/**
+ *    author:  yuyuan
+ *    created: 2025-12-22 22:08:55
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s, t;
+  cin >> s >> t;
+  const int n = s.size(), m = t.size();
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+    }
+  }
+
+  return 0;
+}
+
