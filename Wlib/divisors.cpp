18a8d04d59ff1e000cbbf72f09f2ad7ff3cf18d3
ruojun wang <yuyuan12138x@gmail.com>
2026-03-11 15:08:23 +0800

    a

--- Wlib/divisors.cpp
+++ Wlib/divisors.cpp
@@ -1,5 +1,5 @@
+vector<vector<int>> divs(n + 1);
 void get_divisors(const int n) {
-    std::vector<std::vector<int>> divs(n + 1);
     for (int i = 1; i <= n; i++) {
         for (int j = i; j <= n; j += i) {
             divs[j].push_back(i);
